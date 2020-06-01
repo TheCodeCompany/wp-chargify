2f91c95fcb77f38e5ee1ec3afb41b40ab69f2b54
Bronson Quick <bronson@bronsonquick.com.au>
2020-06-01 11:57:48 +1000

    Finish initial helper functions

--- plugins/wp-chargify/includes/helpers/options.php
+++ plugins/wp-chargify/includes/helpers/options.php
@@ -8,23 +8,51 @@ function get_production_api_key() {
 		return CHARGIFY_PRODUCTION_API_KEY;
 	}
 
-	$production_key = cmb2_get_option( 'settings','chargify_production_API_key' );
+	$production_key = cmb2_get_option( 'chargify_settings', 'chargify_production_API_key' );
 
 	return $production_key;
 }
 
 function get_production_subdomain() {
+	# The Chargify PHP constants will take precedence over the stored options to assist in development.
+	if ( defined( 'CHARGIFY_PRODUCTION_SUBDOMAIN' ) ) {
+		return CHARGIFY_PRODUCTION_SUBDOMAIN;
+	}
 
+	$production_subdomain = cmb2_get_option( 'chargify_settings', 'chargify_production_subdomain' );
+
+	return $production_subdomain;
 }
 
 function get_test_api_key() {
+	# The Chargify PHP constants will take precedence over the stored options to assist in development.
+	if ( defined( 'CHARGIFY_TEST_API_KEY' ) ) {
+		return CHARGIFY_TEST_API_KEY;
+	}
 
+	$test_key = cmb2_get_option( 'chargify_settings', 'chargify_test_API_key' );
+
+	return $test_key;
 }
 
 function get_test_subdomain() {
+	# The Chargify PHP constants will take precedence over the stored options to assist in development.
+	if ( defined( 'CHARGIFY_TEST_SUBDOMAIN' ) ) {
+		return CHARGIFY_TEST_SUBDOMAIN;
+	}
 
+	$test_subdomain = cmb2_get_option( 'chargify_settings', 'chargify_test_subdomain' );
+
+	return $test_subdomain;
 }
 
 function get_mode(){
+	# The Chargify PHP constants will take precedence over the stored options to assist in development.
+	if ( defined( 'CHARGIFY_MODE' ) ) {
+		return CHARGIFY_MODE;
+	}
 
+	$chargify_mode = cmb2_get_option( 'chargify_settings', 'chargify_mode' );
+
+	return $chargify_mode;
 }
--- plugins/wp-chargify/tests/includes/helpers/test-options.php
+++ plugins/wp-chargify/tests/includes/helpers/test-options.php
@@ -5,10 +5,11 @@ class Test_Option_Helpers extends WP_UnitTestCase {
 	public function setUp(){
 		parent::setUp();
 
+		# Setup some random options to test against.
 		$chargify_options = [
-			'chargify_production_API_key'   => 'c66318b166ad76bdffbf980e4203e159c4650f5591dbc9acd6992d1cbdff4bf0',
+			'chargify_production_API_key'   => '345345354t3erertsdfdsfg',
 			'chargify_production_subdomain' => 'https://productionsubdomain.chargify.com',
-			'chargify_test_API_key'         => 'Vrc6WuZTZBZc0ORr5vcKuIwdTNDBH5adjmvFfzWwQWU',
+			'chargify_test_API_key'         => '6787867867dfsdfgsfg',
 			'chargify_test_subdomain'       => 'https://testsubdomain.chargify.com',
 			'chargify_mode'                 => 'test'
 		];
@@ -17,8 +18,31 @@ class Test_Option_Helpers extends WP_UnitTestCase {
 	}
 
 	function test_get_production_api_key() {
-		# Test empty option.
-		$api_key = Options\get_production_api_key();
-		$this->assertFalse( $api_key );
+		$production_api_key = Options\get_production_api_key();
+		$this->assertEquals( '345345354t3erertsdfdsfg', $production_api_key );
+	}
+
+	function test_get_production_subdomain() {
+		$production_subdomain = Options\get_production_subdomain();
+		$this->assertEquals( 'https://productionsubdomain.chargify.com', $production_subdomain );
+	}
+
+	function test_get_test_api_key() {
+		$test_api_key = Options\get_test_api_key();
+		$this->assertEquals( '6787867867dfsdfgsfg', $test_api_key);
+	}
+
+	function test_get_test_subdomain() {
+		$test_subdomain = Options\get_test_subdomain();
+		$this->assertEquals( 'https://testsubdomain.chargify.com', $test_subdomain );
+	}
+
+	function test_chargify_mode() {
+		$chargify_mode = Options\get_mode();
+		$this->assertEquals( 'test', $chargify_mode );
+	}
+
+	public function tearDown(){
+		delete_option( 'chargify_settings' );
 	}
 }
