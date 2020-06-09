c3d8fd63431da9ae1ee47d6b4763bbd91548829f
Bronson Quick <bronson@bronsonquick.com.au>
2020-06-09 14:02:34 +1000

    Update Product CPT tests

--- plugins/wp-chargify/tests/includes/post-types/test-chargify-product.php
+++ plugins/wp-chargify/tests/includes/post-types/test-chargify-product.php
@@ -1,7 +1,15 @@
 <?php
 class Test_Chargify_Product extends WP_UnitTestCase {
-		function test_chargify_product_cpt(){
-			$custom_post_types = get_post_types();
-			$this->assertArrayHasKey('chargify_product', $custom_post_types );
-		}
+	function setUp() {
+		add_filter( 'chargify_show_products', __return_true() );
+	}
+
+	function test_chargify_product_cpt(){
+		$custom_post_types = get_post_types();
+		$this->assertArrayHasKey('chargify_product', $custom_post_types );
+	}
+
+	function test_chargify_account_filter() {
+		$this->assertTrue( has_filter( 'chargify_show_products' ) );
+	}
 }
