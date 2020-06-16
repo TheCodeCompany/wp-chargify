9b7a48837588057b264f90d2c91ca85cc0be0646
Bronson Quick <bronson@bronsonquick.com.au>
2020-06-16 16:34:41 +1000

    Add renewal success functionality

--- plugins/wp-chargify/includes/chargify/renewal/namespace.php
+++ plugins/wp-chargify/includes/chargify/renewal/namespace.php
@@ -1,10 +1,12 @@
 <?php
 namespace Chargify\Renewal;
+use Chargify\Helpers\Customers;
 
 function renewal_success( $payload ) {
-	# Get User ID
-	# Find account with user ID and product ID equal to product
-	# Update current_period_ends_at field.
+	$account_details = Customers\get_account_details_from_email( $payload['subscription']['customer']['email'] );
+	# Update expiration date.
+	update_post_meta( $account_details->post->ID, 'chargify_subscription_status', sanitize_text_field( $payload['subscription']['state'] ) );
+	update_post_meta( $account_details->post->ID, 'chargify_expiration_date', sanitize_text_field( $payload['subscription']['current_period_ends_at'] ) );
 }
 
 function renewal_failure( $payload ) {
