711b71f08474834288515fe33b70f8d4ebcb95f1
Anthony Thorne <anthonythorne33@hotmail.com>
2020-07-07 09:57:13 +1000

    remove the attachment to the window, its not necessary for this class.

--- assets-src/main/js/country-and-states-selectors/index.js
+++ assets-src/main/js/country-and-states-selectors/index.js
@@ -7,10 +7,10 @@ import { CountryStateSelector } from './country-state-selector';
 /**
  * Initialize the country and state selectors.
  */
-$(document).ready(function() {
+$( document ).ready( function() {
 	// Standard address country and state selectors.
-	window.select_country_field = new CountryStateSelector( '#chargify_country', '#chargify_state' );
+	new CountryStateSelector( '#chargify_country', '#chargify_state' );
 
 	// Billing address country and state selectors.
-	window.select_country_field = new CountryStateSelector( '#chargify_billing_country', '#chargify_billing_state' );
-});
+	new CountryStateSelector( '#chargify_billing_country', '#chargify_billing_state' );
+} );
