76c77d889e5300e7325bd351f138a1f7d0753a51
Anthony Thorne <anthonythorne33@hotmail.com>
2020-07-13 08:27:09 +1000

    Update php class name syntax to be consistent

--- plugins/wp-chargify/includes/bootstrap.php
+++ plugins/wp-chargify/includes/bootstrap.php
@@ -47,9 +47,9 @@ function bootstrap() {
 	add_action( 'cmb2_save_field',           'Chargify\\Webhooks\\maybe_toggle_webhooks', 10, 4 );
 
 	// Controllers.
-	new Controllers\AdminPageWPChargifyController();
-	new Controllers\EnqueuesController();
-	new Controllers\ValidateCouponController();
+	new Controllers\Admin_Page_WPChargify_Controller();
+	new Controllers\Enqueues_Controller();
+	new Controllers\Validate_Coupon_Controller();
 }
 
 add_action( 'plugins_loaded', __NAMESPACE__ . '\\bootstrap' );
